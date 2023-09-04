dce904483a4dcb898da286e8fd7df6da7fe25b9a
fahd ahsyani <fahdahsayni@gmail.com>
2023-09-04 17:37:15 +0200

    update 1.0.5

--- src/sections/JoinOurPrograms.jsx
+++ src/sections/JoinOurPrograms.jsx
@@ -1,12 +1,13 @@
 export default function JoinOurPrograms() {
   return (
-    <div className="max-w-7xl mx-auto pb-16 mt-16">
-      <div className="rounded-lg background-mesh py-16 lg:py-32">
-        <div className="container m-auto px-6 text-center md:px-12 lg:px-20">
+    <div className="max-w-6xl mx-auto pb-16 mt-0 lg:mt-16">
+      <div className="rounded-none overflow-hidden relative md:rounded-lg background-mesh py-16 lg:py-32">
+        <div className="w-full h-full absolute top-0 hero-pattern" />
+        <div className="container relative m-auto px-6 text-center md:px-12 lg:px-20">
           <h2 className="mb-8 text-4xl font-bold text-white md:text-6xl">
             How to Join Our Programs ?
           </h2>
-          <p className="text-gray-100 text-lg">
+          <p className="text-gray-100 lg:text-lg">
             <span className="text-white font-bold">
               Take the First Step Towards a Future in Tech:
             </span>
